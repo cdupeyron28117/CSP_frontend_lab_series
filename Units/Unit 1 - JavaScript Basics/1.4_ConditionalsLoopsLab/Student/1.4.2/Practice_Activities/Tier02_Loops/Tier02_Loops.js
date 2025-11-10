e9bb3ae61c42a713306646522246689388ab9b89
cdupeyron28117 <cdupeyron28117@columbushs.com>
2025-11-10 13:43:17 -0500

    Completed Tier 2 and 3 L1.4.2

--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier02_Loops/Tier02_Loops.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier02_Loops/Tier02_Loops.js
@@ -1,2 +1,50 @@
 // Your code goes here. Follow the steps in README.md and run from index.html.
 console.log("TEST - TIER 2")
+
+// Task 6
+for (let i = 1; i <=10; i++) {
+    console.log(i);
+}
+console.log("--------------------");
+
+// Task 7
+let countdown = 10;
+while (countdown >= 0) {
+    console.log(`T-${countdown}`);
+    if (countdown == 0) {
+        console.log("Liftoff!");
+    }
+    countdown--;
+}
+console.log("--------------------");
+
+// Task 8
+let base = Number(prompt("Enter a number:"));
+if (isNaN(base)) {
+    console.log("Please enter a number.");
+} else {
+    for (let x = 1; x <= 10; x++) {
+        console.log(`${base} x ${x} = ${base * x}`);
+    }
+}
+console.log("--------------------");
+
+// Task 10
+let SECRET = "explorers";
+let attempts = 3;
+do {
+    if (prompt("Enter password:") == SECRET) {
+        console.log("Correct!");
+        break;
+    } else {
+        attempts--
+        console.log(`Incorrect! Attempts left: ${attempts}`);
+    }
+} while (attempts > 0)
+console.log("--------------------");
+
+// CHALLENGE: I changed the countdown to include "T-(countdown)".
+
+/* Exit Ticket
+I accidentally used ' instead of ` for template literals. I fixed it by being attentative.
+*/
--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier03_Combining/Tier03_Combining.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier03_Combining/Tier03_Combining.js
@@ -1,2 +1,64 @@
 // Your code goes here. Follow the steps in README.md and run from index.html.
 console.log("TEST - TIER 3")
+
+// Task 11
+for (let i = 1; i <= 50; i++) {
+    if (i % 15 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
+console.log("--------------------");
+
+// Task 12
+function isPrime(Number) {
+    if (Number < 2) return false;
+    for (let x = 2; x <= (Math.sqrt(Number)); x++) {
+        if (Number % x === 0) return false;
+    }
+    return true;
+}
+for (let n = 1; n <= 20; n++) {
+    if (isPrime(n)) {
+        console.log("prime");
+    } else if (n % 2 === 0) {
+        console.log("even");
+    } else {
+        console.log("odd");
+    }
+}
+console.log("--------------------");
+
+// Task 13
+let secret = Math.floor(Math.random() * 10) + 1;
+let guess
+do {
+    guess = Number(prompt("Guess an integer from 1-10"));
+    if (!isNaN(guess) && guess >= 1 && guess <= 10) {
+        if (guess == secret) {
+            console.log("Correct!");
+        } else if (guess > secret) {
+            console.log("Too high!");
+        } else if (guess < secret) {
+            console.log("Too low!");
+        } else {
+            console.log("Enter a valid number!")
+        }
+    }
+} while (guess != secret);
+console.log("--------------------");
+
+// Task 14
+for (let row = 1; row <= 4; row++) {
+    console.log("*".repeat(row));
+}
+console.log("--------------------");
+
+/* Exit Ticket
+I thought that isPrime was a default function. I fixed this by creating the isPrime function.
+*/
